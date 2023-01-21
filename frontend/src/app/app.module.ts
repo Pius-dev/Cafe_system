0b7c0158ba2900e188c7a50efd1b62893e054317
piusobonyo-admin <piusobonyo5@gmail.com>
2023-01-21 19:20:47 +0300

    Sign Up page working well

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxUiLoaderConfig, NgxUiLoaderModule, SPINNER } from 'ngx-ui-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BestSellerComponent } from './best-seller/best-seller.component';
@@ -15,6 +16,17 @@ import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
 import { MaterialModule } from './shared/material-module';
 import { SharedModule } from './shared/shared.module';
 import { SignupComponent } from './signup/signup.component';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  text: 'Loading...',
+  textColor: '#FFFFF',
+  textPosition: 'center-center',
+  bgsColor: '#7bifa2',
+  fgsColor: '#7bifa2',
+  fgsType: SPINNER.threeBounce,
+  fgsSize: 80,
+  hasProgressBar: false,
+};
 
 @NgModule({
   declarations: [
@@ -36,6 +48,7 @@ import { SignupComponent } from './signup/signup.component';
     FlexLayoutModule,
     SharedModule,
     HttpClientModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
   ],
   providers: [],
   bootstrap: [AppComponent],
